fbe8573dbdda2c764c6d0b5b7145011fe0bf9daf
d4nylo <danylo.kucherenko@gmail.com>
2021-05-23 14:47:35 +0100

    Added potusParse.

---
+++ potusParse.js
@@ -0,0 +1,19 @@
+const rp = require('request-promise');
+const cheerio = require('cheerio');
+
+const potusParse = function (url) {
+  rp(url)
+    .then(function (html) {
+      const $ = cheerio.load(html);
+
+      const name = $('#firstHeading').text();
+      const birthday = $('.bday', html).text().substring(0, 10);
+
+      return { name, birthday };
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+}
+
+module.exports = potusParse;
