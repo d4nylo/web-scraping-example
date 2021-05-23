0823dc12b2171f7a18b829eb503e0ea0f526ad92
d4nylo <danylo.kucherenko@gmail.com>
2021-05-23 14:10:24 +0100

    Basic retrieval from the table.

--- potusScraper.js
+++ potusScraper.js
@@ -1,10 +1,14 @@
 const rp = require('request-promise');
+const cheerio = require('cheerio');
 const url = 'https://en.wikipedia.org/wiki/List_of_Presidents_of_the_United_States';
 
 rp(url)
    .then(function (html) {
       //success!
-      console.log(html);
+      //console.log(html);
+      const $ = cheerio.load(html);
+      console.log($('table > tbody > tr > td > b > a', html).length);
+      console.log($('table > tbody > tr > td > b > a', html));
    })
    .catch(function (err) {
       //handle error
